44ec1aa389da7e6a79132d3eb313f4f07e2fbd0e
Gourav Kumar Dhiman <gkrdhiman@gmail.com>
2025-08-08 09:07:17 +0530

    fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Logo from '../Logo'
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../Logo";
 
 function Footer() {
-  return (
-    <section className="relative overflow-hidden py-10 bg-gray-400 border border-t-2 border-t-black">
+    const date = new Date();
+    return (
+        <section className="relative overflow-hidden py-10 bg-gray-400 border border-t-2 border-t-black">
             <div className="relative z-10 mx-auto max-w-7xl px-4">
                 <div className="-m-6 flex flex-wrap">
                     <div className="w-full p-6 md:w-1/2 lg:w-5/12">
@@ -14,7 +15,8 @@ function Footer() {
                             </div>
                             <div>
                                 <p className="text-sm text-gray-600">
-                                    &copy; Copyright 2023. All Rights Reserved by DevUI.
+                                    &copy; Copyright {date.getFullYear()}. All
+                                    Rights Reserved by DevUI.
                                 </p>
                             </div>
                         </div>
@@ -137,7 +139,7 @@ function Footer() {
                 </div>
             </div>
         </section>
-  )
+    );
 }
 
-export default Footer
+export default Footer;
